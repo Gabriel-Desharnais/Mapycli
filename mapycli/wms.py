d06f9786180e27688b001b6f3c63eee6750996bc
gabriel-desharnais <gabriel.desharnais@hotmail.com>
2018-06-16 17:39:38 -0400

    documentation complience

--- mapycli/wms.py
+++ mapycli/wms.py
@@ -16,48 +16,88 @@ class struct(object):
 def addlayers(layerDictList):
 	# This fuction returns a list of layers and sublayers and fill the
 	# information in a struct class
-	layers = []
+	layer = []
 	for layerDict in layerDictList:
-		layers.append(struct())
+		layer.append(struct())
 		# Get title (There can only be one)
-		layers[-1].title = layerDict["Title"][0]
+		layer[-1].title = layerDict["Title"][0][2]
 		# Add name Optional
 		try:
 			# Get name (There can only be one)
-			layers[-1].name = layerDict["Name"][0]
+			layer[-1].name = layerDict["Name"][0][2]
 		except KeyError:
 			# If there is no name, do nothing
 			pass
 		# O CRS
 		try:
-			layers[-1].crs = layerDict["CRS"]
+			layer[-1].crs = [tup[2] for tup in layerDict["CRS"]]
 		except KeyError:
 			# If there is no crs, do nothing
 			pass
 
 		# O Abstract (0/1)
 		try:
-			layers[-1].abstract = layerDict["Abstract"][0]
+			layer[-1].abstract = layerDict["Abstract"][0][2]
 		except KeyError:
 			# If there is no abstract, do nothing
 			pass
 
 		# O KeywordList (0/1)
 		try:
-			layers[-1].keywordlist = layerDict["KeywordList"][0]["Keyword"]
+			layer[-1].keywordlist = [tup[2] for tup in layerDict["KeywordList"][0][0]["Keyword"]]
 		except KeyError:
 			# If there is no keywords, do nothing
 			pass
-		# Add sub layers, if any, to the layers
+
+		# exGeographicBoundingBox
+		layer[-1].exGeographicBoundingBox = struct()
 		try:
-			layers[-1].layers = addlayers(layerDict["Layer"])
+			layer[-1].exGeographicBoundingBox.westBoundLongitude = float(  layerDict["EX_GeographicBoundingBox"][0][0]["westBoundLongitude"][0][2] )
+			layer[-1].exGeographicBoundingBox.eastBoundLongitude = float( layerDict["EX_GeographicBoundingBox"][0][0]["eastBoundLongitude"][0][2] )
+			layer[-1].exGeographicBoundingBox.southBoundLongitude = float( layerDict["EX_GeographicBoundingBox"][0][0]["southBoundLongitude"][0][2] )
+			layer[-1].exGeographicBoundingBox.northBoundLongitude = float( layerDict["EX_GeographicBoundingBox"][0][0]["northBoundLongitude"][0][2] )
+		except:
+			pass
+
+		# Add boundingBox
+		# Create the list of struct
+		layer[-1].boundingBox = []
+		try:
+			for bbox in layerDict["BoundingBox"]:
+				# Create the structure
+				layer[-1].boundingBox.append(struct())
+
+				# Add crs from attributes to struct
+				layer[-1].boundingBox[-1].crs = bbox[1]["CRS"]
+
+				# Add minx, maxx, miny, maxy from attributes to struct
+				layer[-1].boundingBox[-1].minx = float( bbox[1]["minx"] )
+				layer[-1].boundingBox[-1].miny = float( bbox[1]["miny"] )
+				layer[-1].boundingBox[-1].maxx = float( bbox[1]["maxx"] )
+				layer[-1].boundingBox[-1].maxy = float( bbox[1]["maxy"] )
+
+				# Try to add resx and resy
+				try:
+					layer[-1].boundingBox[-1].resx = float( bbox[1]["resx"] )
+					layer[-1].boundingBox[-1].resy = float( bbox[1]["resy"] )
+				except:
+					# If you can't, don't create these variable
+					pass
 		except KeyError:
-			# If there is no layer, do nothing
 			pass
 
 
-	return layers
-	pass
+		# Add sub layers, if any, to the layers
+		try:
+			layer[-1].layer = addlayers([tup[0] for tup in layerDict["Layer"]])
+		except KeyError as e:
+			# If there is no layer, do nothing
+			layer[-1].layer = e
+			pass
+
+
+	return layer
+
 def explore(root):
 	# This function will explore the xml and return a dictionary
 	theXML = {}
@@ -73,14 +113,28 @@ def explore(root):
 			# If it is not registered, do it
 			theXML[name] = []
 
-		# Add the element in the registered tag
-		# if the element as no child add the text
-		if len(list(tag)) == 0:
-			theXML[name].append(tag.text)
-		else:
-			theXML[name].append(explore(tag))
+		# Retrive the values to create tuple associated with the tag
 
+		# Get childs
+		if len(list(tag)) > 0:
+			# If the element has child add keep them in a dict
+			child = explore(tag)
+		else:
+			# Create an empty dict
+			child = {}
+
+		# Get Attributes
+		attributes = tag.attrib
+
+		# Get text of tag
+		text = tag.text
+
+		# Create a tuple and add it to the list of this tag name
+		theXML[name].append( (child,attributes,text) )
+
+	# Return the dict containing the xml struc from this point
 	return theXML
+
 class WMS:
 	# This class implement the method necessary to do request to a wms server
 	defaultVersion = "1.3.0"
@@ -137,7 +191,7 @@ class getCapabilitiesObject:
 			# This dict will contain every variable and list every child
 			self.getCapDict=explore(self.root)
 
-			# If there is the standard describe that a field can only be there once, The next part will just use the first one and ignore the rest
+			# If the standard describe that a field can only be there once, The next part will just use the first one and ignore the rest
 
 			# Create the GetCapabilities structure
 			self.getCapStruct = struct()
@@ -149,11 +203,11 @@ class getCapabilitiesObject:
 
 			# Filling of the service metadata
 			# Get name (There can only be one)
-			self.getCapStruct.service.name = self.getCapDict["Service"][0]["Name"][0]
+			self.getCapStruct.service.name = self.getCapDict["Service"][0][0]["Name"][0][2]
 			# Get title (There can only be one)
-			self.getCapStruct.service.title = self.getCapDict["Service"][0]["Title"][0]
+			self.getCapStruct.service.title = self.getCapDict["Service"][0][0]["Title"][0][2]
 			# Get abstract (There can only be one)
-			self.getCapStruct.service.abstract = self.getCapDict["Service"][0]["Abstract"][0]
+			self.getCapStruct.service.abstract = self.getCapDict["Service"][0][0]["Abstract"][0][2]
 			# No OnlineResource
 			# Contact information should be added
 			# Optional info
@@ -167,11 +221,10 @@ class getCapabilitiesObject:
 
 			# O Fill layers arg with a list of layers
 			try:
-				self.getCapStruct.capability.layers = addlayers(self.getCapDict["Capability"][0]["Layer"])
+				self.getCapStruct.capability.layer = addlayers([tup[0] for tup in self.getCapDict["Capability"][0][0]["Layer"]])
 			except KeyError:
 				# If there is no layer, do nothing
 				pass
-
 
 
 
